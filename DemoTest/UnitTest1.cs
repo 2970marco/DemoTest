83ab950a8da43d6e490f083625ff9f6733d6de96
2970marco <2970marco@gmail.com>
2024-12-22 19:44:40 +0200

    Add project files.

---
+++ DemoTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using Allure.NUnit;
+
+namespace DemoTest;
+
+[AllureNUnit]
+[TestFixture]
+public class Tests
+{
+    ApiDemoInfra api = new ApiDemoInfra();
+    string apiUrl = "http://localhost/demo/WeatherForecast/";
+
+    [Test]
+    public void WeatherForecastTest([Range(1, 12)] int value)
+    {
+        var res = api.GetWebApi($"{apiUrl}{value}");
+    }
+}
---
+++ DemoTest/WeatherForecast.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoTest;
+
+public class WeatherData
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF { get; set; }
+
+    public string Summary { get; set; }
+}
+
+public class WeatherDataClass
+{
+    public int Id { get; set; }
+    public string Status { get; set; }
+    public string Method { get; set; }
+    public List<WeatherData> Result { get; set; }
+}
